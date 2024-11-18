9600f2f36428adb611ee86a462e89cfa90287ca4
Marko Kotnik <herqapp@gmail.com>
2024-11-18 10:13:21 +0100

    Initial Commit

---
+++ DogBreedApp/DogBreedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedAppApp.swift
+//  DogBreedApp
+//
+//  Created by ANDRAZ TULJAK on 18. 11. 24.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DogBreedAppTests/DogBreedAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  DogBreedAppTests.swift
+//  DogBreedAppTests
+//
+//  Created by ANDRAZ TULJAK on 18. 11. 24.
+//
+
+import Testing
+
+struct DogBreedAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
